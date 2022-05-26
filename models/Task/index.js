b14714db6ba893a7a5622757b043ee81b0ababcc
thevsd <thevsd27@gmail.com>
2022-05-26 21:25:50 +0700

    Task API ver 1

--- models/Task/index.js
+++ models/Task/index.js
@@ -2,54 +2,60 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const taskSchema = new Schema({
-  assign: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
+    assign: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
 
-  name:{
-    type: String,
-    required: [true, "Task Name is required"]
-  },
+    team: {
+        type: Schema.Types.ObjectId,
+        ref: 'Team',
+        required: true
+    },
 
-  start_time: {
-    type: Date,
-    required: true
-  },
-
-  end_time: {
-    type: Date,
-    required: true
-  },
-
-  executor: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-
-  files: [
-    {
+    name: {
         type: String,
-    }
-  ],
+        required: [true, "Task Name is required"]
+    },
 
-  comments: [
-    {
+    type: {
+        type: Number, // 0 - Todo, 1 - Pending, 2 - Finished
+        required: [true, "Task Type is required"]
+    },
+
+    start_time: {
+        type: Date,
+        required: true
+    },
+
+    end_time: {
+        type: Date,
+        required: true
+    },
+
+    executor: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+
+    files: [{
+        type: String,
+    }],
+
+    comments: [{
         comment: String,
-        user:{
+        user: {
             type: Schema.Types.ObjectId,
             ref: 'User',
         }
-    }
-  ],
+    }],
 
-  description: String,
-},
-{
-  collection: 'users',
-  timestamps: true
+    description: String,
+}, {
+    collection: 'users',
+    timestamps: true
 });
 
 const Task = mongoose.model('Task', taskSchema);
---
+++ services/taskService/index.js
@@ -0,0 +1,98 @@
+const createError = require("http-errors");
+// const { translateAliases } = require("../../models/User");
+
+const Task = require('../../models/Task');
+const User = require('../../models/User');
+
+
+const getAllTasks = async({ user }) => {
+    var allTasks = new Array();
+    const tasks = await Task.find({});
+    tasks.map(task => {
+        if (task.team == team._id) {
+            allTasks.push(task);
+        }
+    })
+    return allTasks;
+}
+
+//get team's detail
+const getTask = async({ taskId }) => {
+    try {
+        const task = await Task.findById(taskId);
+        return task;
+    } catch (error) {
+        throw createError(error.statusCode || 500, error.message);
+    }
+};
+
+// create new team
+const createTask = async({
+    userId,
+    teamId,
+    taskName,
+    taskType,
+    startTime,
+    endTime,
+    executorId
+}) => {
+    try {
+        assigner = userId.toString();
+        const newTask = new Task({
+            assign: assigner,
+            team: teamId,
+            name: taskName,
+            type: taskType,
+            start_time: startTime,
+            end_time: endTime,
+            executor: executorId
+        });
+        const task = await newTask.save();
+
+        return task;
+    } catch (error) {
+        throw createError(error.statusCode || 500, error.message);
+    }
+};
+
+//update team's detail
+const updateTask = async({ taskId, taskName, taskType, startTime, endTime, executorId }) => {
+    try {
+        let task = await Task.findById(taskId);
+
+        task.name = taskName;
+        task.type = taskType;
+        task.start_time = startTime;
+        task.end_time = endTime;
+        task.executor = executorId;
+
+        await task.save();
+        return {
+            success: true,
+            message: 'Updated task successfully'
+        };
+    } catch (error) {
+        throw createError(error.statusCode || 500, error.message);
+    }
+};
+
+//remove team
+const removeTask = async({ taskId }) => {
+    try {
+        const task = await Task.findByIdAndRemove(taskId)
+        return {
+            success: true,
+            message: 'Removed task successfully'
+        };
+    } catch (error) {
+        throw createError(error.statusCode || 500, error.message);
+    }
+}
+
+module.exports = {
+    getTask,
+    createTask,
+    getAllTasks,
+    updateTask,
+    removeTask,
+};
