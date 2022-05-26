bc1731c3a1966e1f4c106d64182e74da753591db
thevsd <thevsd27@gmail.com>
2022-05-27 00:44:00 +0700

    Fix router for task

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const authRouter = require('./authRoutes');
 const userRouter = require('./userRoutes');
 const teamRouter = require('./teamRoutes');
+const taskRouter = require('./taskRoutes');
 const friendRouter = require('./friendRoutes');
 const conversationRouter = require('./conversationRoutes');
 
@@ -10,9 +11,9 @@ const router = express.Router();
 
 // Render Home page
 router.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'Welcome to Tism services API'
-  });
+    res.status(200).json({
+        message: 'Welcome to Tism services API'
+    });
 });
 
 // Routes for authentication
@@ -23,6 +24,10 @@ router.use('/user', userRouter);
 
 // Routes for team
 router.use('/team', teamRouter);
+
+// Routes for task
+router.use('/task', taskRouter);
+
 // Routes for friend
 router.use('/friends', friendRouter);
 
@@ -30,22 +35,22 @@ router.use('/friends', friendRouter);
 router.use('/conversations', conversationRouter);
 
 router.post('/socket/test', (req, res) => {
-  const { userId } = req.body;
-  io.to(userId).emit('test-socket', {
-    success: true,
-    message: 'abc'
-  });
-  // console.log(io);
-  res.status(200).json({
-    success: true
-  });
+    const { userId } = req.body;
+    io.to(userId).emit('test-socket', {
+        success: true,
+        message: 'abc'
+    });
+    // console.log(io);
+    res.status(200).json({
+        success: true
+    });
 });
 
 // Response not found with url not match
 router.use('/*', (req, res, next) => {
-  res.status(400).json({
-    message: 'No service provided for this Endpoint'
-  });
+    res.status(400).json({
+        message: 'No service provided for this Endpoint'
+    });
 });
 
 module.exports = router;
